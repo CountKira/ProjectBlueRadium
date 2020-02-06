d164628f7b336aa3852b31c027b0c78f18944966
Stefan Riedmiller <19615804+CountKira@users.noreply.github.com>
2020-02-06 22:58:32 +0100

    Slaying any enemy is now displayed

--- BusinessLogic/BusinessLogic/Game.cs
+++ BusinessLogic/BusinessLogic/Game.cs
@@ -182,10 +182,15 @@ namespace BusinessLogic
 		{
 			var damage = Player.Equipment.Any(i => i.HasTag(Tag.Weapon)) ? 2 : 1;
 			DealDamageToCreature(damage, creature);
-			if (creature.HealthPoints <= 0 && creature.HasTag(Tag.GameEnd))
+			if (creature.HealthPoints <= 0)
 			{
-				writer.WriteTextOutput("You have slain the final enemy. A winner is you.");
-				IsRunning = false;
+				writer.WriteTextOutput($"You have slain the {creature.Name}.");
+
+				if (creature.HasTag(Tag.GameEnd))
+				{
+					writer.WriteTextOutput("You have slain the final enemy. A winner is you.");
+					IsRunning = false;
+				}
 			}
 		}
 
